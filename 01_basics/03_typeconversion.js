6f6a4ee2a2f5e7d76a6b66e9f6e5c79c50c7c359
SwayamPrasad710 <112013673+SwayamPrasad710@users.noreply.github.com>
2024-07-19 18:02:09 +0000

    type conversion done

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,38 @@
+//***  NUMBER CONVERSION ***// 
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valNumber = Number(score);
+// console.log(typeof valNumber);
+// console.log(valNumber); // NaN -> not a number
+
+// "33" => 33
+// "33abc" => NaN - not a number
+// true => 1; false=> 0;
+// "" => 0
+// null => 0
+// undefined => NaN
+
+///*** BOOLEAN CONVERSION ***///
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 || n > 1 => true; 0 => false;
+// "" => false;
+// "swayam" => true
+
+let randomInt = undefined
+
+let stringNum = String(randomInt);
+console.log(stringNum);
+console.log(typeof stringNum);
+
+// 33 => "33" as a string
+// NaN => Nan
+// null => null as a string
+// undefined => undefined as a string
