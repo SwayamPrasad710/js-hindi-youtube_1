6466eaf4d7422e8aa9b798c77ad007cc6ee3507f
SwayamPrasad710 <112013673+SwayamPrasad710@users.noreply.github.com>
2024-07-22 17:41:17 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+// let myName = 'Swayam';
+// console.log(myName.slice(-1, 6));
+
+let str = '      Swayam     |\n|';
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd());
+
+// read split, replace, charAt, IndexO, etc, etc.
