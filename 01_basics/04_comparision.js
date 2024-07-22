ff3fad6c0bcfeff057f7997a2625e69053b5437f
SwayamPrasad710 <112013673+SwayamPrasad710@users.noreply.github.com>
2024-07-22 16:12:02 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,6 @@
+/*
+ equality check == and comparision > < >= <= works differently. Comparision convert null to a pointer and treating it as 0. Hence, ( null >= 0 ) o/p : true but ( null > 0 ) is false.
+
+*/
+
+// (== normal equality check) / (=== strict check.)
